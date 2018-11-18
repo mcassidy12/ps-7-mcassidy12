49cb7c56c62f0386ddab42a9dde2cdcb7e7e6a0f
mauracassidy <mcass@uw.edu>
2018-11-18 11:53:15 -0800

    added to homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,7 +3,10 @@ import { Jumbotron } from 'reactstrap';
 
 export class Homepage extends Component {
     render() {
-        return <h1>Maura Cassidy</h1>
-        
+        return <div>
+            <h1>Maura Cassidy</h1>
+            <p>This is a webpage about Maura Cassidy made using react. Enjoy!</p>
+            <p>Maura would like to thank Mark Zuckerberg for this opportunity.</p>
+        </div>
     }
 }
